ff561059644efbf7a7e1946307d0a67cc955279f
pradhumnamalakar <32121890+pradhumnamalakar@users.noreply.github.com>
2018-09-15 20:28:45 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,75 @@
+//$(document).ready(function(){  
+//(function($) {          
+//    $(document).ready(function(){                    
+//        $(window).scroll(function(){                          
+//            if ($(this).scrollTop() > 200) {
+//                $('.navbar').fadeIn(500);
+//            } else {
+//                $('.navbar').fadeOut(500);
+//            }
+//        });
+//    });
+//})(jQuery);
+//
+//$(window).scroll(
+//    {
+//        previousTop: 0
+//    }, 
+//    function () {
+//    var currentTop = $(window).scrollTop();
+//    if (currentTop < this.previousTop) {
+////        $(".sidebar em").text("Up");
+//        $(".navbar").fadeIn(1000).show();
+//    } else {
+////        $(".sidebar em").text("Down");
+//        $(".navbar").hide();
+//    } 
+//    this.previousTop = currentTop;
+//});
+
+if ( $(window).width() > 769 ) {   
+var mywindow = $(window);
+//    console.log(mywindow);
+var mypos = mywindow.scrollTop();
+var up = false;
+var newscroll;
+mywindow.scroll(function () {
+    newscroll = mywindow.scrollTop();
+    if (newscroll > mypos && !up) {
+        $('.navbar').fadeOut(800);
+//        $('.bottom-navbar').fadeOut(800);
+        up = !up;
+        console.log(up);
+    } else if(newscroll < mypos && up) {
+        $('.navbar').stop().fadeIn(400);
+//        $('.bottom-navbar').stop().fadeIn(400);
+        up = !up;
+    }
+    mypos = newscroll;
+});
+}
+if ( $(window).width() <= 576) {   
+var mywindow = $(window);
+//    console.log(mywindow);
+var mypos = mywindow.scrollTop();
+var up = false;
+var newscroll;
+mywindow.scroll(function () {
+    newscroll = mywindow.scrollTop();
+    if (newscroll > mypos && !up) {
+//        $('.navbar').fadeOut(800);
+        $('.bottom-navbar').fadeOut(800);
+        up = !up;
+        console.log(up);
+    } else if(newscroll < mypos && up) {
+//        $('.navbar').stop().fadeIn(400);
+        $('.bottom-navbar').stop().fadeIn(400);
+        up = !up;
+    }
+    mypos = newscroll;
+});
+}
+
 var TxtType = function(el, toRotate, period) {
         this.toRotate = toRotate;
         this.el = el;
@@ -54,3 +126,4 @@ var TxtType = function(el, toRotate, period) {
         css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
         document.body.appendChild(css);
     };
+//});
