98ab4f3d2c3775ecf472a9e77a3aa70ca1da4e4c
Pradhumna <pradhumnamalakar@gmail.com>
2024-10-20 02:01:56 +0530

    fix: Update heading subheading and resume

--- js/index.js
+++ js/index.js
@@ -64,7 +64,7 @@ var TxtType = function(el, toRotate, period) {
         this.toRotate = toRotate;
         this.el = el;
         this.loopNum = 0;
-        this.period = parseInt(period, 10) || 000;
+        this.period = parseInt(period, 10) || 0;
         this.txt = '';
         this.tick();
         this.isDeleting = false;
